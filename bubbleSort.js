523a6b3acb3b273e0e825179f24683cd905f069e
andrielb93 <andriel.bueno93@gmail.com>
2023-11-29 20:02:04 -0300

    first commit

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,41 +1,52 @@
-let numeros = [6,3,8,0,2,7,1,9,5,4]
-let cont = 0
-let cont2 = numeros.length
-let i1=0
-let i2=1
+var numeros = [6,3,8,0,2,7,1,9,5,4]
+var cont = 0
+var i1=0
+var i2=1
+var verifica = numeros.slice(0)
+
+function compare (arr1,arr2){
+  return arr1.length === arr2.length &&
+    arr1.every((item,index) => item === arr2[index])
+}
 
 console.log(numeros)
 
-compara(i1,i2,cont,cont2)
+compara(i1,i2,cont)
 
-  function compara(i1,i2,cont,cont2){
+function compara(i1,i2,cont){
 
-      let c1 = numeros[i1]
-      let c2 = numeros[i2]
+  var c1 = numeros[i1]
+  var c2 = numeros[i2]
 
   if (numeros[i1] > numeros[i2]){
     numeros[i1] = c2
     numeros[i2] = c1
   }
 
-    i1++
-    i2++
-    cont++
+  i1++
+  i2++
+  cont++
 
-    console.log(cont)
-    console.log(cont2)
-    console.log(numeros)
+  console.log(cont)
+  console.log(numeros)
+  console.log(verifica)
+ 
 
-    if(cont<numeros.length-1){
-    compara(i1,i2,cont,cont2)
+  if(cont < (numeros.length-1)){
+    console.log("caiu no if")
+    
+    compara(i1,i2,cont)
+  } else {
+    console.log("caiu no else")
+    i1 = 0
+    i2 = 1
+    cont = 0
+    console.log(verifica !== numeros)
+    
+    if (compare(verifica,numeros)) {
+      compara(i1,i2,cont)
+    } else {
+      console.log("terminou")
     }
-    else{
-        cont2--
-        i1 = 0
-        i2 = 1
-        cont = 0
-        if (cont2 > 0) {
-            compara(i1,i2,cont,cont2)
-        }
-    }
+  }
 }
